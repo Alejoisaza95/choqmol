edfee7eb221c9910aea1cbcce9782c4923f3a396
Alejandro Isaza <alejomed_11@hotmail.com>
2022-03-30 01:31:41 -0500

    1:31AM

--- Leer.php
+++ Leer.php
@@ -9,9 +9,7 @@ if(!isset($_POST['Buscar'])){
 
 $buscar = $_POST["Buscar"];
 
-$sql_leer = "SELECT * FROM clientes WHERE Nombre LIKE '%".$buscar."%'  
-OR Apellido LIKE '%".$buscar."%' OR Direccion LIKE '%".$buscar."%'  OR Tipo_Cliente LIKE '%".$buscar."%'
-OR Estado_Cliente LIKE '%".$buscar."%'";
+$sql_leer = "SELECT * FROM clientes WHERE Nombre LIKE '%".$buscar."%'  OR Apellido LIKE '%".$buscar."%'";
 
 $result = mysqli_query($conexion,$sql_leer);
 if(!$result){
